457ee95386360d08f7ac7c95e407266ecb8d7bd2
Bryce <brycelogue@gmail.com>
2021-09-03 17:09:06 -0400

    Revert "Revert "Pre v1.0.0""
    This reverts commit d69fe95c28e121a5690925bf62e87d616cc969de.

--- util/mongo.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose')
-const { mongoPath } = require('../config/config.json')
-
-module.exports = async () => {
-  mongoose.connect(mongoPath, {
-    useUnifiedTopology: true,
-    useNewUrlParser: true
-  })
-}
